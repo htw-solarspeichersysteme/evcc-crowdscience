d885bc9716ce0393124daf9338614e4f727bbbda
Lukas Frey <lukas.frey03@outlook.de>
2025-09-19 16:01:32 +0200

    add migration script to container

---
+++ apps/web/drizzle/migrate.ts
@@ -0,0 +1,10 @@
+import { Database } from "bun:sqlite";
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { migrate } from "drizzle-orm/bun-sqlite/migrator";
+
+import { env } from "~/env";
+
+const sqlite = new Database(env.DATABASE_PATH);
+const db = drizzle(sqlite);
+
+migrate(db, { migrationsFolder: "./drizzle" });
