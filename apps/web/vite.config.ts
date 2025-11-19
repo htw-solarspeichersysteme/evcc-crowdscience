b696b08e9d468946b25fb2a8aa24597b9fa762e8
Lukas Frey <hey@lukasfrey.com>
2025-11-19 18:24:25 +0100

    try fixing ci again

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -39,6 +39,6 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
-    analyzer({ openAnalyzer: false }),
+    analyzer({ enabled: process.env.ANALYZE_BUNDLE === "true" }),
   ],
 });
